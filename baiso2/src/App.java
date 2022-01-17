d589209f564d783231f13358115797354a7d66ba
lvdzz <97437316+lvdzz@users.noreply.github.com>
2022-01-17 18:56:17 +0700

    Add files via upload

---
+++ baiso2/src/App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner; 
+ưpublic class App {
+public static void main(String[] args) throws Exception {
+Scanner sc = new Scanner(System.in);
+String strHoTen = ""; 
+System.out.println("Nhap ho ten: ");
+strHoTen strHoTen = sc.nextLine(); 
+System.out.println("Xin chao " + strHoTen);
+
