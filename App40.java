4097738ae524418031d28ca475b533464cf3ef4a
lvdzz <97437316+lvdzz@users.noreply.github.com>
2022-02-28 21:52:55 +0700

    Add files via upload

---
+++ App40.java
@@ -0,0 +1,44 @@
+importjava.until.Scanner;
+
+public class SwitchDemo {​
+
+public static void main(String agre[]) {​
+
+Scanner scan=newScanner (system.in);​
+
+int ngay=scan.nextInt();
+
+nguyên​
+
+switch(ngay){​
+
+Case 0:system.out.println("Chunhat");​
+
+        break;​
+        Case 1:system.out.println("Thu hai");​
+
+        break;​
+
+Case 2:system.out.println("Thu ba");​
+
+        break;​
+
+Case 3:system.out.println(“Thu tu”);​
+
+        break;​
+
+Case 4:system.out.println("Thu nam");​
+
+        break;​
+
+Case 5:system.out.println("Thu sau");​
+
+        break;​
+
+Case 6:system.out.println("Thu bay");​
+
+        break;​
+
+default:system.out.println("So ngay trong tuan sai");​
+
+}}}​
